05a551e10fbb0a5a94dad9bec8d1fc2c9bdd613c
king-hypertext <oseikin7@gmail.com>
2023-10-30 16:05:48 +0000

    assets folder got updated

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default"
+    data-assets-path="../assets/" data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="favicon.png" />
+    <link rel="stylesheet" href="{{ url('assets/vendor/fonts/boxicons.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ url('assets/vendor/css/theme-default.css') }}"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ url('assets/css/demo.css') }}" />
+    <link rel="stylesheet" href="{{ url('assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+    <script src="{{ url('assets/vendor/js/helpers.js') }}"></script>
+    <script src="{{ url('assets/js/config.js') }}"></script>
+    <script src="{{ url('assets/vendor/jQuery.js') }}"></script>
+    <title>Q-Pharma | PHARMACY MANAGEMENT SYSTEM</title>
+</head>
+
+<body>
+
+
+    <script src="{{ url('assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ url('assets/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ url('assets/vendor/js/menu.js') }}"></script>
+</body>
+
+</html>
